5781be637b2ef3a8b1ae47e076c1a07cf7975d3d
BrunoRodri <fidbruno@hotmail.com>
2017-02-21 17:19:48 -0300

    Create ProdutoDao.java

---
+++ ProdutoDao.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Produto {
+
+	public static void main(String[] args) {
+		
+		Scanner leitor = new.Scanner(System.in);
+		
+		public void cadastrar (Produto produto);
+		
+		System.out.println("Digite o id do produto:");
+		int id = leitor.nextInt();
+		
+		System.out.println("Digite o nome do produto:");
+		String nome = leitor.nextLine();
+		
+		System.out.println("Digite o tipo do produto:");
+		String tipo = leitor.nextLine();
+		
+		System.out.println("Digite a marca do produto:");
+		String Marca = leitor.nextLine();
+		
+		System.out.println("Digite a descrição do produto");
+		String descrição = leitor.nextLine();
+		
+
+	}
+
+}
