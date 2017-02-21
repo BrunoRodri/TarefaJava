730d3d61fde33c3849cec4da210e056dd7098e7d
BrunoRodri <fidbruno@hotmail.com>
2017-02-21 17:18:42 -0300

    Create Produto.java

---
+++ Produto.java
@@ -0,0 +1,68 @@
+
+
+public class CadastroProduto {
+	
+	private int id;
+	private String nome;
+	private String tipo;
+	private String marca;
+	private String descrição;
+	
+	
+	
+	
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public String getTipo() {
+		return tipo;
+	}
+
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+
+	public String getMarca() {
+		return marca;
+	}
+
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+
+	public String getDescrição() {
+		return descrição;
+	}
+
+
+	public void setDescrição(String descrição) {
+		this.descrição = descrição;
+	}
+
+
+	
+	
+
+}
